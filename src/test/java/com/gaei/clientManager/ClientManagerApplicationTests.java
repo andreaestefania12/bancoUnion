14d0b70643b9d5de4ba92e66f291f674767e965f
Andrea Timaran <andreaestefania12@hotmail.com>
2025-09-23 20:41:50 -0500

    Implement hexagonal architecture for ClientManager

---
+++ src/test/java/com/gaei/clientManager/ClientManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaei.clientManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
