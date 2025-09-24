ff527646400620c4644c686c56ee614c6b259f10
Andrea Timaran <andreaestefania12@hotmail.com>
2025-09-24 02:21:59 -0500

    feat: Add validation to prevent duplicate clients

--- src/main/java/com/gaei/clientManager/application/service/ClientService.java
+++ src/main/java/com/gaei/clientManager/application/service/ClientService.java
@@ -4,6 +4,7 @@ import com.gaei.clientManager.application.ports.in.CreateClientUseCase;
 import com.gaei.clientManager.application.ports.in.FindClientUseCase;
 import com.gaei.clientManager.application.ports.out.ClientRepository;
 import com.gaei.clientManager.domain.exception.ClientNotFoundException;
+import com.gaei.clientManager.domain.exception.ClientValidationException;
 import com.gaei.clientManager.domain.model.Client;
 import com.gaei.clientManager.domain.service.ClientValidatorService;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,12 @@ public class ClientService implements CreateClientUseCase, FindClientUseCase {
     public Client saveClient(Client client){
         ClientValidatorService validatorService = new ClientValidatorService();
         validatorService.validateClient(client);
+        String documentType = client.getDocumentType().getType();
+        String documentNumber = client.getDocumentNumber();
+        if(clientRepository.findByDocumentNumberAndType(documentNumber, documentType).isPresent()){
+            String fullMessage = "Cliente "+ documentType + " " + documentNumber + ". Ya se encuentra registrado";
+            throw new ClientValidationException(fullMessage, client.getIdTx());
+        }
         return clientRepository.save(client);
     }
 
